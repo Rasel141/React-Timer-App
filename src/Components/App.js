dfffd451db9f3ac2cfed8ee64cd2563526301c61
Rasel <rrasel141@gmail.com>
2018-04-03 01:24:38 +0600

    Completed Heading Part

---
+++ src/Components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Title from './Title/Title';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-sm-8 offset-sm-2">
+              <Title />
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
